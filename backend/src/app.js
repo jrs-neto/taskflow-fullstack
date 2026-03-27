d467efcad02dd82a1d9b54e8c88b320ea2806b33
jrs-neto <jrd.santosneto@gmail.com>
2026-03-27 17:50:33 -0300

    feat: create tasks route and integrate with express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const taskRoutes = require("./routes/taskRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
